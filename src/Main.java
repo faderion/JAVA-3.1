02f57924ab89f16b4769df0b590285e28642eed7
Faderion <faderion215@gmail.com>
2024-05-08 16:15:24 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double heightCM = 1.87; // Рост в см , дробное число
+        double heightCM = 1.87; // Рост в метрах , дробное число
         int weight = 98; // Масса в кг, целое число
         int index = service.calculate(weight, heightCM); //
         System.out.println(index);
